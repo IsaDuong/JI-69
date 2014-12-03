e9d24473559d96f73e66f617931c98cca0ea5ed8
IsaDuong <s491151@student.roundrockisd.org>
2014-12-03 16:01:32 -0600

    hell fucking yes this better fucking work

--- JI-69.py
+++ JI-69.py
@@ -2,11 +2,23 @@
 from Tkinter import *
 root = Tk()
 
-drawpad = Canvas(root, width=400,height=950, background='grey')
+drawpad = Canvas(root, width=400,height=725, background='grey')
 drawpad.grid(row=0, column=1)
 
+screen = drawpad.create_rectangle(50,50,350,250, fill = "white")
+#Buttons 1-9
+button0 = drawpad.create_rectangle(112.5,650,162.5,675, fill="white")
+button1 = drawpad.create_rectangle(112.5,612.5,162.5,637.5, fill="white")
+button2 = drawpad.create_rectangle(175,612.5,225,637.5, fill="white")
+button3 = drawpad.create_rectangle(237.5,612.5,287.5,637.5, fill="white")
+button4 = drawpad.create_rectangle(112.5,575,162.5,600, fill="white")
+button2 = drawpad.create_rectangle(175,612.5,225,637.5, fill="white")
+button2 = drawpad.create_rectangle(175,612.5,225,637.5, fill="white")
+button2 = drawpad.create_rectangle(175,612.5,225,637.5, fill="white")
+
+
 class MyApp:
-	def __init__(self, parent):
+    	def __init__(self, parent):
 	        global drawpad
 		self.myParent = parent  
 		self.myContainer1 = Frame(parent)
@@ -19,16 +31,15 @@ class MyApp:
 		self.button2 = Button(self.myContainer1)
 		self.button2.configure(text="2", background= "white")
 		self.button2.grid(row=0,column=1)
+	
+
 		
 										
 
 
 		drawpad.pack()
 		
-
-		
 myapp = MyApp(root)
-
 
 
 root.mainloop()
